55707422368271d20bb6d223d237c5ac9532d535
Shivam <shivamnegi840@gmail.com>
2022-01-01 20:02:46 +0530

    Finished 75% of Course

--- app.js
+++ app.js
@@ -5,8 +5,56 @@ var person = {
 console.log(person.name);
 var students = ['shubham', 'shivam', 5];
 var i = 0;
-var data = ['shubham', 19]; // Tuple
+var data = ['shubham', 19]; // Tuple is for defining data structure
 do {
     console.log(students[i]);
     i++;
 } while (i < students.length);
+// Understanding Enums Typescript
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+// 0 ,      1,        2
+var humans = {
+    name: 'shubham',
+    role: Role.READ_ONLY
+};
+if (humans.role === Role.ADMIN) {
+    console.log("Is admin");
+}
+else {
+    console.log('Not admin');
+}
+;
+// Any type  
+// const students: any[] = ['shubham', 'shivam', 5];
+// ------------>   Union Type
+function combine(input1, input2, input3) {
+    var result;
+    if (input1 === 'number' && input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    ;
+    return result;
+}
+;
+var combineAges = combine(30, 20, 'negi');
+var combineNames = combine('Max', 'Sam', 'suvi');
+var TypeAlises = 'suvi';
+// ----------------> Function return and Voids
+function setReturnType(n1, n2) {
+    return n1.toString() + n2.toString();
+}
+// Function that doesn't return anything call Void Functions
+function voidFunction(num) {
+    console.log('Result: ' + num);
+}
+;
+// undefined is a type in TypeScript
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-const person: {name: string, age: number} = {
+const person: {name: string, age: number} = {  // Describing the type after colun(:)
     name: 'shubham',
     age: 40,
 };
@@ -9,7 +9,7 @@ console.log(person.name);
 const students: any[] = ['shubham', 'shivam', 5];
 let i = 0;
 
-const data: [string, number] = ['shubham', 19]; // Tuple
+const data: [string, number] = ['shubham', 19]; // Tuple is for defining data structure
 
 do {
     console.log(students[i]);
@@ -19,5 +19,64 @@ do {
 
 // Understanding Enums Typescript
 
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+          // 0 ,      1,        2
 
+const humans = {
+    name: 'shubham',
+    role: Role.READ_ONLY,
+};
+
+if (humans.role === Role.ADMIN) {
+    console.log("Is admin");
+} else {
+    console.log('Not admin')
+};
+
+
+
+// Any type  
+// const students: any[] = ['shubham', 'shivam', 5];
+
+
+// ------------>   Union Type
+
+function combine(input1: string | number, input2: string | number, input3: 'suvi' | 'negi') { // uinion type  // literals:- input3: 'suvi' | 'negi'
+    let result;
+    if(input1 === 'number' && input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    };
+
+    return result;
+};
+
+const combineAges = combine(30, 20, 'negi');
+
+const combineNames = combine('Max', 'Sam', 'suvi');
+
+
+// ---------------> type Alises
+
+type Combinable = number | string;
+type FixedStrings = 'Suvi' | 'negi';
+
+const TypeAlises: Combinable = 'suvi';
+
+
+// ----------------> Function return and Voids
+
+function setReturnType(n1: number, n2: number): string {
+    return n1.toString() + n2.toString();
+}
+
+
+// Function that doesn't return anything call Void Functions
+
+function voidFunction(num: number): void {
+    console.log('Result: '+ num);
+};
+
+// undefined is a type in TypeScript
 
